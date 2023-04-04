3a406464c07a9031eb95f555df9d063f34e8afb8
VincentBtr <vin.bertrand@gmail.com>
2023-04-04 18:46:31 +0200

    new texts, new Lyd pic, add mail address and spam prevention

--- index.js
+++ index.js
@@ -40,4 +40,15 @@ function switchLang() {
   }
 }
 
+// Mixing mail
+// document.getElementById("addr").innerHTML = "<a href='mailto:test@test.com'>&#x69;&#x6E;&#x3C;&#x73;&#x70;&#x61;&#x6E;&#x20;&#x63;&#x6C;&#x61;&#x73;&#x73;&#x3D;&#x22;&#x68;&#x69;&#x64;&#x65;&#x22;&#x3E;&#x6E;&#x66;&#x3C;&#x2F;&#x73;&#x70;&#x61;&#x6E;&#x3E;&#x66;&#x3C;&#x21;&#x2D;&#x2D;&#x69;&#x6E;&#x40;&#x6B;&#x66;&#x2D;&#x2D;&#x3E;&#x6F;&#x40;&#x62;&#x72;&#x75;&#x73;&#x73;&#x65;&#x6C;&#x73;&#x3C;&#x21;&#x2D;&#x2D;&#x69;&#x6E;&#x40;&#x6B;&#x66;&#x2D;&#x2D;&#x3E;&#x73;&#x6F;&#x75;&#x6E;&#x64;&#x74;&#x72;&#x61;&#x63;&#x6B;&#x6F;&#x72;&#x63;&#x68;&#x65;&#x73;&#x74;&#x72;&#x61;&#x3C;&#x73;&#x70;&#x61;&#x6E;&#x20;&#x63;&#x6C;&#x61;&#x73;&#x73;&#x3D;&#x22;&#x68;&#x69;&#x64;&#x65;&#x22;&#x3E;&#x24;&#x3C;&#x2F;&#x73;&#x70;&#x61;&#x6E;&#x3E;&#x2E;&#x62;&#x3C;&#x73;&#x70;&#x61;&#x6E;&#x20;&#x63;&#x6C;&#x61;&#x73;&#x73;&#x3D;&#x22;&#x68;&#x69;&#x64;&#x65;&#x22;&#x3E;&#x7A;&#x3C;&#x2F;&#x73;&#x70;&#x61;&#x6E;&#x3E;&#x65;</a>";
+let address = "&#x69;&#x6E;&#x66;&#x6F;&#x40;&#x62;&#x72;&#x75;&#x73;&#x73;&#x65;&#x6C;&#x73;&#x73;&#x6F;&#x75;&#x6E;&#x64;&#x74;&#x72;&#x61;&#x63;&#x6B;&#x6F;&#x72;&#x63;&#x68;&#x65;&#x73;&#x74;&#x72;&#x61;&#x2E;&#x62;&#x65;";
+let addressEnc = "aW5mb0BicnVzc2Vsc3NvdW5kdHJhY2tvcmNoZXN0cmEuYmU=";
+$(".addr").html(address);
+function openMail() {
+  window.location.href = "mailto:" + atob(addressEnc);
+}
 
+// $("#mail").click(function () {
+//   switchLang();
+// })
